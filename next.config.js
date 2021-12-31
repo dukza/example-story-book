7d5863852ecab889f861fc106506c260b941d037
dukza <dyha7648@gmail.com>
2021-12-31 19:17:47 +0900

    page

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
     path: "https://dukza.github.io/example-story-book",
   },
   reactStrictMode: true,
+  webpack: (config, {
+    buildId,
+    dev,
+    isServer,
+    defaultLoaders,
+    webpack
+ }) => {
+    // Important: return the modified config
+    return config
+ },
 }
